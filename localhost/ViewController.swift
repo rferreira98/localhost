a22d2b1fe951b99d39315f967f56d8b1d52aa118
Ricardo <r_ferreira@outlook.pt>
2019-10-25 16:40:47 +0100

    Initial commit 2

---
+++ localhost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  localhost
+//
+//  Created by Ricardo Filipe Ribeiro Ferreira on 25/10/2019.
+//  Copyright © 2019 Ricardo Filipe Ribeiro Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
