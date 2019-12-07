effcba3f5f29b2b4b1f6e35950a6d7e8b4a7d8fc
Mike <miguelsousa.14@gmail.com>
2019-12-07 01:03:16 +0000

    Added filter by city

--- controllers/FiltersTableViewController.swift
+++ controllers/FiltersTableViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class FiltersTableViewController: UITableViewController, CLLocationManagerDelegate {
+class FiltersTableViewController: UITableViewController, UITextFieldDelegate, CLLocationManagerDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
     @IBOutlet weak var sliderDistance: UISlider!
     @IBOutlet weak var lblDistance: UILabel!
+    @IBOutlet weak var textFieldPickerLocal: UITextField!
+    
+    var localPicker = UIPickerView()
+    var selectedCity: String = ""
     
     let locationManager = CLLocationManager()
     
@@ -29,9 +33,17 @@ class FiltersTableViewController: UITableViewController, CLLocationManagerDelega
             locationManager.requestWhenInUseAuthorization()
             locationManager.requestLocation()
         }
+
+        createPicker();
         
+        textFieldPickerLocal.delegate = self
         
         verifyMetricUnit()
+        
+        //Dismiss keyboard with a tap.
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
     }
     
     
@@ -70,19 +82,22 @@ class FiltersTableViewController: UITableViewController, CLLocationManagerDelega
     @IBAction func onClickApplyFilterButtonClick(_ sender: Any) {
         print("locations = \(lastLocationObj!.latitude) \(lastLocationObj!.longitude)")
         
-        if UserDefaults.standard.integer(forKey: "metricUnit") == 0 {
-            //print("current miles radius: \(currentRadiusValue * 1000)")
-            getLocals(currentLocationLatitude: lastLocationObj!.latitude, currentLocationLongitude: lastLocationObj!.longitude, radius: Double(currentRadiusValue) * 1000)
-        }else if UserDefaults.standard.integer(forKey: "metricUnit") == 1 {
-            var currentRadius = 0.0
-            if currentRadiusValue >= 24 {
-                currentRadius = 24.0 * 1609.34
-            } else {
-                currentRadius = Double(currentRadiusValue) * 1609.34
+        if currentRadiusValue != 1 {
+            if UserDefaults.standard.integer(forKey: "metricUnit") == 0 {
+                //print("current miles radius: \(currentRadiusValue * 1000)")
+                getLocals(currentLocationLatitude: lastLocationObj!.latitude, currentLocationLongitude: lastLocationObj!.longitude, radius: Double(currentRadiusValue) * 1000)
+            }else if UserDefaults.standard.integer(forKey: "metricUnit") == 1 {
+                var currentRadius = 0.0
+                if currentRadiusValue >= 24 {
+                    currentRadius = 24.0 * 1609.34
+                } else {
+                    currentRadius = Double(currentRadiusValue) * 1609.34
+                }
+                getLocals(currentLocationLatitude: lastLocationObj!.latitude, currentLocationLongitude: lastLocationObj!.longitude, radius: currentRadius)
             }
-            getLocals(currentLocationLatitude: lastLocationObj!.latitude, currentLocationLongitude: lastLocationObj!.longitude, radius: currentRadius)
-        }
-        
+         } else if selectedCity != "" {
+             
+         }
         
     }
     
@@ -124,6 +139,12 @@ class FiltersTableViewController: UITableViewController, CLLocationManagerDelega
         }
     }
     
+    private func getLocalsByCity(city: String){
+        
+        //FALTA FAZER CHAMADA A API ETC
+    }
+    
+    
     private func verifyMetricUnit() {
         if UserDefaults.standard.integer(forKey: "metricUnit") == 0 {
             sliderDistance.value = 1
@@ -134,6 +155,79 @@ class FiltersTableViewController: UITableViewController, CLLocationManagerDelega
             sliderDistance.maximumValue = 25
             lblDistance.text = "1 mile"
         }
+    }
+    
+    //Local picker methods
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return Cities.cities.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return Cities.cities[row]
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedCity = Cities.cities[row]
+        textFieldPickerLocal.text = selectedCity
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+
+        var label: UILabel
+
+        if let view = view as? UILabel {
+            label = view
+        } else {
+            label = UILabel()
+        }
+
+        
+        label.textAlignment = .center
+        label.text = Cities.cities[row]
+
+        return label
+    }
+    
+    func createPicker() {
+        
+        
+        localPicker.delegate = self
+        textFieldPickerLocal.inputView = localPicker
+        localPicker.showsSelectionIndicator = true
+        
+        let toolBar = UIToolbar()
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor(red: 76/255, green: 217/255, blue: 100/255, alpha: 1)
+        toolBar.sizeToFit()
+
+        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.done, target: self, action: #selector (self.dimissPicker))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItem.Style.plain, target: self, action: #selector (self.dimissPickerReset))
+
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        
+        textFieldPickerLocal.inputAccessoryView = toolBar
+      
+
+    }
+    @objc func dimissPicker(){
+        self.view.endEditing(true)
+    }
+    @objc func dimissPickerReset(){
+        self.view.endEditing(true)
+        resetPicker()
+    }
+    func resetPicker() {
+        self.textFieldPickerLocal.text = ""
+        self.selectedCity = ""
+        
     }
 }
 
